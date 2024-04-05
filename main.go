ef028de3bf905f60136f85acdf1cd706830f2c11
Travis Cline <travis.cline@gmail.com>
2024-04-04 17:59:17 -0700

    protoc-gen-gotag: Set support for optional fields

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	pgs "github.com/lyft/protoc-gen-star"
 	pgsgo "github.com/lyft/protoc-gen-star/lang/go"
+	"google.golang.org/protobuf/types/pluginpb"
 
 	"github.com/brevdev/protoc-gen-gotag/module"
 )
 
 func main() {
-	pgs.Init(pgs.DebugEnv("GOTAG_DEBUG")).
+	opt := uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
+	pgs.Init(pgs.DebugEnv("GOTAG_DEBUG"), pgs.SupportedFeatures(&opt)).
 		RegisterModule(module.New()).
 		RegisterPostProcessor(pgsgo.GoFmt()).
 		Render()
